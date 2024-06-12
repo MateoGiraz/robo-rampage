4a2be542f78b966767ab30d28d014932ed977d02
MateoGiraz <mateogiraz27@gmail.com>
2024-06-12 15:33:58 -0300

    add plugin

---
+++ Plugins/GameViewportLocalMultiplayer/Intermediate/Build/Win64/UnrealGame/Inc/GameViewportLocalMultiplayer/UHT/GameViewportLocalMultiplayer.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGameViewportLocalMultiplayer_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_GameViewportLocalMultiplayer;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_GameViewportLocalMultiplayer()
+	{
+		if (!Z_Registration_Info_UPackage__Script_GameViewportLocalMultiplayer.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/GameViewportLocalMultiplayer",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xD4DC70B7,
+				0xF5A57234,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GameViewportLocalMultiplayer.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_GameViewportLocalMultiplayer.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameViewportLocalMultiplayer(Z_Construct_UPackage__Script_GameViewportLocalMultiplayer, TEXT("/Script/GameViewportLocalMultiplayer"), Z_Registration_Info_UPackage__Script_GameViewportLocalMultiplayer, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD4DC70B7, 0xF5A57234));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
